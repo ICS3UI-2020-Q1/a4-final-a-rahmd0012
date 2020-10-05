204abf257bb2ac3fbd043b0a6b6acc28ed7d3b17
rahmd0012 <71138020+rahmd0012@users.noreply.github.com>
2020-10-05 12:00:28 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * a program that determine which numbers divide a user entered number evenly
+ * @author Daniel Rahmani
  */
 public class Main {
 
@@ -9,7 +11,31 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for a positive integer
+    System.out.println("Please enter a positive integer to determine its factors:");
+    int n = input.nextInt();
+
+    // create a variable to count up a number 
+    int count = 0;
+
+    // make sure the number is a positive integer
+    while (n <= 0){
+    // Ask for a new number
+    System.out.println("Please enter a positive integer");
+    n = input.nextInt();
+    } 
+    System.out.println("The factors of " + n +" are:");
+
+    // find out the factors of the number
+    do{
+      count = count + 1;
+    if(n%count == 0.0){
+      System.out.println(count);
+    } 
+    }while (count < n);
     
   }
 }
